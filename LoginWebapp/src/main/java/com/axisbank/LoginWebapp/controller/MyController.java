ad6282b1d72132169024794bb366df3a8aa47d8c
shrivatsakoulgi <89825785+shrivatsakoulgi@users.noreply.github.com>
2022-06-15 17:15:22 +0530

    Add files via upload

---
+++ LoginWebapp/src/main/java/com/axisbank/LoginWebapp/controller/MyController.java
@@ -0,0 +1,25 @@
+package com.axisbank.LoginWebapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MyController {
+
+	@RequestMapping("/login")
+	public ModelAndView login(@RequestParam("username") String userName, 
+			@RequestParam("password") String password) {
+
+		ModelAndView mv = new ModelAndView();		
+		mv.setViewName("response.jsp");
+		if(userName.equals("admin") && password.equals("root")) {
+			mv.addObject("status", true);
+		}
+		else {
+			mv.addObject("status", false);
+		}
+		return mv;
+	}
+}
